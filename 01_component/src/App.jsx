1eea9ea05013ef18a5d902c61588f158ccdafb56
rishav <goyalrishav20@gmail.com>
2024-01-15 18:45:31 +0530

    deriving data based on state conditional rendering, and dynamic styling

--- 01_component/src/App.jsx
+++ 01_component/src/App.jsx
@@ -1,22 +1,22 @@
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import { useState } from 'react';
 
 function App() {
   //react state hook will return an array with two elements and destructing.
-  const [selectedTopic,setTopic] = useState('please click a button'); 
+  const [selectedTopic, setTopic] = useState();
 
   console.log('App component executing');
 
   // event handler implementation in react
   function clickHandler(selected_component) {
-   setTopic(selected_component); //react Hooks method
-    console.log(selectedTopic);
+    setTopic(selected_component); //react Hooks method - app fucntion called again
+    console.log(selectedTopic); // print previous state as at this stage we are previous app fucntion
   }
 
-  
+
   return (
     <>
       <Header />
@@ -27,7 +27,7 @@ function App() {
             {/* custom component attribute props - passing attribute props using spread operator 
             and object destrucring method */}
             {CORE_CONCEPTS.map(concept =>
-              <CoreConcept {...concept} />
+              <CoreConcept key={concept.title} {...concept} />
             )}
 
           </ul>
@@ -37,12 +37,30 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>clickHandler("Components")}>Components</TabButton>
-            <TabButton onSelect={()=>clickHandler("JSX")}>JSX</TabButton>
-            <TabButton onSelect={()=>clickHandler("Props")}>Props</TabButton>
-            <TabButton onSelect={()=>clickHandler("State")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components' ? true : undefined}
+              onSelect={() => clickHandler("components")}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx' ? true : undefined} 
+            onSelect={() => clickHandler("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props' ? true : undefined} 
+            onSelect={() => clickHandler("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state' ? true : undefined} 
+            onSelect={() => clickHandler("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+
+          {/* accessing content on button click of tab pane and showing repective content using state hook */}
+          {!selectedTopic ?  /* conditional rendering */
+            <p>Please select a topic</p>
+            :
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          }
         </section>
       </main>
     </>
--- 01_component/src/components/TabButton.jsx
+++ 01_component/src/components/TabButton.jsx
@@ -1,9 +1,10 @@
-export default function TabButton({children,onSelect}) {
+export default function TabButton({children,onSelect,isSelected}) {
     console.log('TabButton component executing')
     return (
         // click event apply
+        //applying dynamic styling on button
         <li>
-            <button onClick={onSelect}>{children}</button>  
+            <button className={isSelected?'active':undefined} onClick={onSelect}>{children}</button>  
         </li>
     );
 }
